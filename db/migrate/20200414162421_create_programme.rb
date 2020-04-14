0ab66d995b3325a86ed997d3e4816c230b8c82b0
Jen <jenniferanndcb@gmail.com>
2020-04-14 23:03:31 +0100

    removed all ref to programme

--- db/migrate/20200414162421_create_programme.rb
+++
@@ -1,9 +0,0 @@
-class CreateProgramme < ActiveRecord::Migration[6.0]
-  def change
-    create_table :programmes do |t|
-      t.string :title
-      t.string :url
-      t.string :description
-    end
-  end
-end
